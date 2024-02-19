5142e88224e293421cbd86c9e4588aa77452542d
EthicalXGamer <155059899+EthicalXGamer@users.noreply.github.com>
2024-02-19 21:50:02 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = int(getenv("API_ID", ""))
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
-    FSUB = getenv("FSUB", "Venom_Chatz")
+    FSUB = getenv("FSUB", "ethicalmusicsupport")
     CHID = int(getenv("CHID", ""))
     SUDO = list(map(int, getenv("SUDO", "5871038439").split()))
     MONGO_URI = getenv("MONGO_URI", "")
